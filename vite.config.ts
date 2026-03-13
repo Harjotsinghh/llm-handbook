f2bb75bec76ab513fa241c68cec4248d58701161
Harjot Singh <harjotsingh206@gmail.com>
2026-03-13 22:49:15 +0530

    use base path for non github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/llm-handbook/",
+  base: isGithubPages ? "/llm-handbook/" : "/",
 });
